53a234b3dc2d1b5e576c9d89ccd5a0a0b43e116b
jackDino18 <143567695+jackDino18@users.noreply.github.com>
2023-09-14 09:46:06 -0400

    initial commit

---
+++ Doingmath.py
@@ -0,0 +1,22 @@
+from random import random 
+from math import sqrt, acos
+def main():
+    #tells user to enter x coordinate
+    x_user:int = input("enter the x coordinate")
+
+    #tells user to enter y coordinate
+    y_user:int = input("enter y coordinate")
+
+    
+    #creates random x coordinate
+    x_enemy:int = random.seed(100)
+    print(x_enemy)
+
+    #creates random y coordinate
+    y_enemy:int = random.seed(100)
+
+
+
+
+
+
